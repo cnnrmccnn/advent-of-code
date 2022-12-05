cb100b3f2e0ef4898ece4b20efb15d392b406723
cnnrmccnn <n9711678@qut.edu.au>
2022-12-05 19:17:03 +1000

    day 4

---
+++ 2022/04/src/a.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from 'fs';
+
+function findNumbersBetween(range: string) {
+  const minAndMax = range.split('-');
+  const list: number[] = [];
+  for (let i = parseInt(minAndMax[0]); i <= parseInt(minAndMax[1]); i++) {
+    list.push(i);
+  }
+  return list;
+}
+
+const p1 = function () {
+  const text = readFileSync('./2022/04/data/input.txt', 'utf8');
+  const assignments = text.split('\n');
+  let value: number = 0;
+  assignments.forEach((elfPair) => {
+    const elves = elfPair.split(',');
+    let elfOneNumbers = findNumbersBetween(elves[0]);
+    let elfTwoNumbers = findNumbersBetween(elves[1]);
+    let elfOneMatch = elfOneNumbers.every((job) => elfTwoNumbers.includes(job));
+    let elfTwoMatch = elfTwoNumbers.every((job) => elfOneNumbers.includes(job));
+    if (elfOneMatch || elfTwoMatch) {
+      value++;
+    }
+  });
+  console.log(value);
+};
+
+p1();
